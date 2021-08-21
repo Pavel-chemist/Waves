fda649d30ed659d711a8777ede5f7e0a0dca006a
Pavel-chemist <Pavel-chemist@mail.ru>
2021-08-21 18:34:46 +0300

    1 Created pi calculator based on basic pendulum

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ $(function()
 	
 	
 	
+	
+	
 	let linewidth = 4;
 	let raywidth = 1;
 	let lshift = -0;
@@ -75,8 +77,77 @@ $(function()
 	
 	let controls = 
 	{
-		'placeholderControl':			function() { console.log('placeholder message'); },
+		'calculate_Pi':	function() { console.log('button "Get Pi" was clicked'); PiCalc(); }
 	};
+	
+	
+	function CalculatePiWithPendulum (step) 
+	{/*
+		basic idea:
+		X(t0) = 0, V(t0) = 1;
+		X(t1) = X(t0) + step * V(t0);
+		V(t1) = V(t0) - step * X(t0);
+		calculate until X(t_i) < 0;
+		find t(X(t) = 0) using last two values;
+		*/
+		
+	//	let step = 0.001;
+		let X0 = 0, X1, V0 = 1, V1;
+		let t0 = 0, t1;
+		let calculatedPi = 0;
+		
+		X1 = X0 + step * V0;
+		V1 = V0 - step * X0;
+		t1 = t0 + step;
+		
+		while (X1 >= 0)
+		{
+			X0 = X1;
+			V0 = V1;
+			t0 = t1;
+			
+			X1 = X0 + step * V0;
+			V1 = V0 - step * X0;
+			t1 = t0 + step;
+		}
+		
+		let a = (X1 - X0) / step;
+		let b = X0 - a * t0;
+		calculatedPi = -b / a;
+		
+		return calculatedPi;
+	
+	}
+	
+	function PiCalc ()
+	{
+		console.log('PiCalc is working');
+	//	let $value = $('#calc_pi_val');
+		$('#calc_pi_real').text(Math.PI);
+		let $StepSize = $('#step_size');
+		let $OutputValue = $('#calc_pi_val');
+		let $ErrorDisplay = $('#calc_pi_error');
+		
+		let step = +$StepSize.val();
+		let calculatedPiValue;
+		
+		console.log('step is ', step);
+		
+		if (step > 0 && step <= 1)
+		{
+			calculatedPiValue = CalculatePiWithPendulum(step);
+			
+			$OutputValue.text(calculatedPiValue);
+			$ErrorDisplay.text(calculatedPiValue - Math.PI);
+		}
+		else
+		{
+			console.log('step size should be between 0 and 1, excluding 0');
+		}
+		
+	}
+	
+	
 	
 	// DrawSpectrometerDiagram( DiagramCtx );
 	
@@ -243,8 +314,6 @@ $(function()
 		//point.x, point.y;
 		//pivot.x, pivot.y, pivot.angle
 		
-		
-	//	let radAngle = - AngleConvert( pivot.angle, 'toRad' );
 		let radAngle = - pivot.angle;
 		
 		let rotatedPoint = 
@@ -316,9 +385,27 @@ $(function()
 	//	$('#pivotAngleDisplay').text( (pivot.angle/deg).toFixed(0) );
 	}
 	
+	function FindInList( id )
+	{
+		switch(id)
+		{
+			case 'calculate_Pi': 
+			return true;
+
+			default: return false;
+		}
+	}
+	
 	function ClickHandler( $target )
 	{	
-		console.log('click was somewhere');
+	//	console.log('click was on', $target.attr('id'));
+	//	console.log('input is: ', $StepSize.val());
+		if (FindInList($target.attr('id')))
+		{
+			controls[$target.attr('id')]();	
+		}
+		
+	//	CalculatePiWithPendulum();
 	//	DrawSpectrometerDiagram( DiagramCtx );		
 	}
 	
